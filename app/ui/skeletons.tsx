803e445dca75c5edc1fb8aad02f60fa4e672aba2
HaimSarraf <ali.assar1988@gmail.com>
2024-10-08 17:38:20 +0330

    before adding authentication

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 // Loading animation
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
@@ -17,7 +19,6 @@ export function CardSkeleton() {
     </div>
   );
 }
-
 export function CardsSkeleton() {
   return (
     <>
@@ -28,7 +29,6 @@ export function CardsSkeleton() {
     </>
   );
 }
-
 export function RevenueChartSkeleton() {
   return (
     <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
@@ -43,7 +43,6 @@ export function RevenueChartSkeleton() {
     </div>
   );
 }
-
 export function InvoiceSkeleton() {
   return (
     <div className="flex flex-row items-center justify-between border-b border-gray-100 py-4">
@@ -58,7 +57,6 @@ export function InvoiceSkeleton() {
     </div>
   );
 }
-
 export function LatestInvoicesSkeleton() {
   return (
     <div
@@ -81,7 +79,6 @@ export function LatestInvoicesSkeleton() {
     </div>
   );
 }
-
 export default function DashboardSkeleton() {
   return (
     <>
@@ -101,7 +98,6 @@ export default function DashboardSkeleton() {
     </>
   );
 }
-
 export function TableRowSkeleton() {
   return (
     <tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
@@ -138,7 +134,6 @@ export function TableRowSkeleton() {
     </tr>
   );
 }
-
 export function InvoicesMobileSkeleton() {
   return (
     <div className="mb-2 w-full rounded-md bg-white p-4">
@@ -162,7 +157,6 @@ export function InvoicesMobileSkeleton() {
     </div>
   );
 }
-
 export function InvoicesTableSkeleton() {
   return (
     <div className="mt-6 flow-root">
